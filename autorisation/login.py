c8222997bd004f73aac77e632f45ecd673fa9231
nonelop <morlksaq@gmail.com>
2026-05-13 19:56:27 +0700

    Drop password function

--- autorisation/login.py
+++ autorisation/login.py
@@ -4,20 +4,13 @@ from connection.server_requests.database import check_username
 def login():
     while True:
         name = input("Введите логигн: ")
-        password = input("Введите пароль: ")
 
         is_exist = check_username(username=name)
 
-        if is_exist == 1:
-            is_password_correct = database.check_password(
-                username=name, password=password
-            )
-
-            if is_password_correct:
+        if is_exist == True:
                 print("Успешная авторизация.\n")
                 return name
-            else:
-                print("Неправильный пароль. попробуйте ещё раз")
+
         else:
             print(
                 "Пользователя с таким именем не существует. попробуйте еще раз, либо используйте регистрацию"
